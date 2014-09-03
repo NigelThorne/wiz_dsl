8c7f758d3b8a4f4c6f41dc27c12bba0c6683e145
Nigel Thorne <github@nigelthorne.com>
2014-09-04 00:16:02 +1000

    Add transformation

--- parser.rb
+++ parser.rb
@@ -44,16 +44,93 @@ class QADSLParser < Parslet::Parser
     }
     rule(:condition) { state }
     rule(:and_op) { sp? >> str('&') >> sp? }
-    rule(:conclusion) { (eol.absent? >> any).repeat(1) }
+    rule(:conclusion) { (eol.absent? >> any).repeat(1).as(:result) }
+end
+
+class MyTransform < Parslet::Transform
+  rule(state: simple(:name)){ builder.state(name) }
+  rule(option: simple(:name)){ builder.option(name) }
+  rule(
+    question_text: simple(:text),
+    options: sequence(:options),
+    states: sequence(:states)
+  ){ builder.question(text, options, states) }
+  rule(question: simple(:questions)){ questions }
+  rule(conditions: simple(:condition), result: simple(:result)){ 
+      builder.rule([condition], result) 
+  }
+  rule(conditions: sequence(:conditions), result: simple(:result)){ 
+      builder.rule(conditions, result) 
+  }
+  rule(rule_def: simple(:rule)){ rule }
+end
+
+class State
+  def initialize(name)
+    @name = name
+  end
+end
+
+class Option
+  def initialize(name)
+    @name = name
+  end
+end
+
+class Question
+  def initialize(text, options, states)
+    @text, @options, @states = text, options, states
+  end
+end
+
+class Rule
+  def initialize(conditions, result)
+    @conditions = conditions
+    @result = result
+  end  
+end
+
+class Builder
+  def initialize
+    @states = {}
+    @options = {}
+    @questions = []
+    @rules = []
+  end
+
+  def state(name)
+    @states[name] ||= State.new(name)
+  end
+
+  def option(name)
+    @options[name] ||= Option.new(name)
+  end
+
+  def question(text, options, states)
+    x = Question.new(text, options, states)
+    @questions << x
+    x
+  end
+  
+  def rule(conditions, state)
+    x = Rule.new(conditions, state)
+    @rules << x
+    x
+  end
 end
 
 if __FILE__==$0
+  require 'pp'
+
   file = File.read(ARGV[0])
   lines = file.split("\n")
   lines.each_with_index{|line,i| puts "#{i+1}: #{line}" }
   parser = QADSLParser.new
+  trans = MyTransform.new
   begin
-    puts parser.parse(file)
+    puts "=============="
+    pp trans.apply(parser.parse(file), builder: Builder.new )
+    puts "=============="
   rescue Parslet::ParseFailed => error
     puts error.cause.ascii_tree
   end
